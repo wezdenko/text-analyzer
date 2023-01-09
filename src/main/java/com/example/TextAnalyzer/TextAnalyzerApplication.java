1d7c91dd225cc20dd6f1839e20cf5cb56d547a5a
Mateusz Wezdeńko <mateusz.wezdenko.stud@pw.edu.pl>
2023-01-09 19:29:48 +0100

    Initial commit

---
+++ src/main/java/com/example/TextAnalyzer/TextAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TextAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAnalyzerApplication.class, args);
+	}
+
+}
